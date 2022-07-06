baacc3e8c8d3fb7d8e7b57a5703f5979b716b582
William Lawrence <lawrence.william86@gmail.com>
2022-07-06 19:10:55 -0400

    Create login session (authentication) with cookies as well as salt and hash passwords

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const bcrypt = require("bcrypt"); // use this for even stronger encryption with hashing and salting rounds
-const saltRounds = 10; // there will be 2^10 rounds of salting the hash.
-
+const session = require('express-session');// peep the session requirement
+const passport = require ("passport"); // peep the passport requirement
+const passportLocalMongoose = require("passport-local-mongoose"); // peep the passpot local mongoose requirement
+//passport-local doesn't need to be required here because we'll never refer to it in the code and plus it already comes inside of passport local mongoose ?
 
 const port = 3000;
 
@@ -17,6 +18,20 @@ console.log(process.env.SECRET); //this will log the secret shit that's in the .
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
+
+
+//Tell the app to use the express-session package from above (IMPORTANT: PUT THIS HERE!!!!![above mongoose connect and below the app.use shit])
+//Documentation--> https://www.npmjs.com/package/express-session
+app.use(session({
+  secret: "Our little secret.",
+  resave: false,
+  saveUninitialized: false
+}));
+
+//Tell the app to use the passport package to initialize the passport package and to also use passport to deal with the session
+//Documentation--> https://www.npmjs.com/package/passport
+app.use(passport.initialize());
+app.use(passport.session());
 
 
 mongoose.connect("mongodb://localhost:27017/userDB");
@@ -31,13 +46,24 @@ var userSchema = new mongoose.Schema({
 });
 
 
-//////////////////////////////CREATE MODEL
 
+//Use this to hash and salt the passwords and to save the users into the mongodb database
+userSchema.plugin(passportLocalMongoose);
+
+
+
+//////////////////////////////CREATE MODEL
 const User = new mongoose.model("User", userSchema);
 
 
 
 
+//create a local strategy to authenticate users using their username and password and to seralize and deserialize the users
+//Documentation --> https://www.npmjs.com/package/passport-local-mongoose
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 
 
@@ -54,6 +80,15 @@ app.get("/register", function(req, res){
 });
 
 
+//Check to see if user is authenticated (logged in) before showing the logged in version of the website
+app.get("/secrets", function (req,res){
+  if (req.isAuthenticated()){ //if the user is logged in...
+    res.render("secrets");
+  }else{ //if the user isn't logge in....
+    res.redirect("/login");
+  }
+});
+
 
 
 
@@ -62,49 +97,52 @@ app.get("/register", function(req, res){
 // Register with even stronger encryption using salting of hashes
 app.post("/register",function(req, res){
 
-  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {  //bcrypt salting code
-
-    const newUser = new User ({
-      email: req.body.username,
-      password: hash //peep the hash
-    });
-
-    newUser.save(function(err){
-      if (err){
-        console.log(err);
-      }else{
-        res.render("secrets");
-      }
-    });
+  User.register({username:req.body.username}, req.body.password, function(err, user) {
+    if (err) {
+      console.log(err);
+      res.redirect("/register");
+    }else{
+      passport.authenticate("local")(req, res, function(){ // this authenticates the user and (creates a login session)
+        res.redirect("/secrets");
+      });
+    }
   });
-
-
 });
 
 
 /////////////////////////////////////////login to app with email and password
 app.post("/login", function(req, res){
-  const username = req.body.username;
-  const password = req.body.password; // peep the hashing
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
+  });
 
-  User.findOne({email: username}, function(err, foundUser){
+  req.login(user, function(err){
     if (err){
       console.log(err);
     }else{
-      if (foundUser){
-        bcrypt.compare(password, foundUser.password, function(err, result) {  //bcrypt salting shit
-          if (result === true){
-            res.render("secrets");
-          }
-        });
-      }
+      passport.authenticate("local")(req, res, function(){
+        res.redirect("/secrets");
+      });
+    }
+  });
+});
+
+
+///////////////////////////////////////logout app by deauthenticating user and ending the session
+app.get("/logout", function (req, res){
+  req.logout(function (err){
+    if (err){
+      console.log(err);
+    }else{
+      res.redirect("/");
     }
   });
 });
 
 
 
-
+//listen on port 3000?
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
